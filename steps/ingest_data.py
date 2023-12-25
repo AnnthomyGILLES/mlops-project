af59a42aadce3319c8481834f2632bc40f79d03f
AnnthomyGILLES <annthomy.gilles@gmail.com>
2023-12-25 14:13:16 -0500

    Add Data ingestion

--- steps/ingest_data.py
+++ steps/ingest_data.py
@@ -12,7 +12,7 @@ class IngestData:
         return pd.read_csv(self.data_path)
 
 @step
-def ingest_data(data_path:str) -> pd.DataFrame:
+def ingest_df(data_path:str) -> pd.DataFrame:
     try:
         df = IngestData(data_path).get_data()
         return df
