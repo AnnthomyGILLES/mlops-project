222c7bb43080bb74b8d78dab8b6944f6afbeeb40
AnnthomyGILLES <annthomy.gilles@gmail.com>
2023-12-27 18:29:54 -0500

    Delete files

--- run_pipeline.py
+++
@@ -1,5 +0,0 @@
-from pipelines.training_pipeline import train_pipeline
-
-if __name__ == '__main__':
-
-    train_pipeline(data_path=r"C:\Users\gotam\PycharmProjects\mlops-project\data\olist_customers_dataset.csv")
