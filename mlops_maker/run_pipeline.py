2bfaaecaf80f61cf25d7e50ea70641e0e1eba632
AnnthomyGILLES <annthomy.gilles@gmail.com>
2023-12-29 11:32:51 -0500

    Delete files

--- mlops_maker/run_pipeline.py
+++
@@ -1,6 +0,0 @@
-from mlops_maker.pipelines.training_pipeline import train_pipeline
-from zenml.client import Client
-
-if __name__ == '__main__':
-    print(Client().active_stack.experiment_tracker.get_tracking_uri())
-    train_pipeline(data_path=r"C:\Users\gotam\PycharmProjects\mlops-project\data\olist_customers_dataset.csv")
